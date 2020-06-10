72d9152fea215d1f168034f6accba0cbeea54bd4
Peng Du <pdu@futurewei.com>
2020-06-10 22:47:08 +0000

    add alias script

---
+++ add-alias.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+alias be-peng='kubectl config use-context futurewei-pengdu-context'
+alias be-xiaoning='kubectl config use-context futurewei-xiaoning-context'
+alias be-tenant-admin='kubectl config use-context futurewei-admin-context'
