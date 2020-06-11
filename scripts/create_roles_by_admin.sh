e4fbec50a26184d52ef42d33df13307362534ce9
Peng Du <pdu@futurewei.com>
2020-06-11 06:45:03 +0000

    a few fixes

--- scripts/create_roles_by_admin.sh
+++ scripts/create_roles_by_admin.sh
@@ -3,7 +3,5 @@
 set -e
 set -x
 
-kubectl create -f futureweirole-reader.yaml 
-kubectl create -f futureweirole-pod-reader.yaml 
-kubectl create -f futureweirolebinding-pengdu.yaml 
-kubectl create -f futureweirolebinding-xiaoning.yaml 
+kubectl create -f role-reader.yaml 
+kubectl create -f rolebinding-reader.yaml 
